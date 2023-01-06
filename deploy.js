11401d8f00e392c9cb20da8d1d1c9d70bf7d51dc
Shubham Jain <someshubham@gmail.com>
2023-01-06 17:51:00 +0530

    add env

--- deploy.js
+++ deploy.js
@@ -2,11 +2,10 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
+require('dotenv').config();
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+  process.env.MNEMONIC,
+  process.env.INFURA_URL,
 );
 const web3 = new Web3(provider);
 
